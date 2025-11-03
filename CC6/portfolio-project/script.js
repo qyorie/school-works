04e9f220ae5c2b48c03e119ae959f4f807ca4d76
Troy Tristan Jacob <qyorie.0707@gmail.com>
2025-11-03 15:26:38 +0800

    added CC6 portflio project

---
+++ CC6/portfolio-project/script.js
@@ -0,0 +1,29 @@
+// smooth scrolling for anchor links
+$(document).ready(function(){
+  const navbarHeight = 80; // set this to your navbar height in pixels
+
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        var hash = this.hash;
+        var targetOffset = $(hash).offset().top - navbarHeight;
+
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 800, function(){
+            
+        });
+    }
+  });
+});
+
+// Scroll listener for navbar opacity
+window.addEventListener("scroll", function() {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
